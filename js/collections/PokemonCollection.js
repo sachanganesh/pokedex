44b855601fc08fcc258dac3589ef3a0558e00eaf
Sachan Ganesh <sachan.ganesh@gmail.com>
2015-01-25 16:17:26 -0800

    Ordered pokemon rendering

--- js/collections/PokemonCollection.js
+++ js/collections/PokemonCollection.js
@@ -1,9 +1,6 @@
 $(function () {
   App.Collection.Pokemon = Backbone.Collection.extend({
     model: App.Model.Pokemon,
-    url: 'http://pokeapi.co/api/v1/pokedex/1/',
-    parse: function (resp) {
-      return resp.pokemon;
-    }
+    url: 'api/pokemon.json'
   });
 });
