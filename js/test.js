e95c29633af0b5be6a09d196293213eabcf3f177
Sachandhan Ganesh <me@sachanganesh.com>
2015-04-03 21:05:25 -0700

    Render pokemon properly

--- js/test.js
+++ js/test.js
@@ -1,4 +1,21 @@
+var template = null;
+
 $(function() {
+	var pokedex = null,
+	pokemonID = 1;
+
+	function initRouter() {
+		var routes = {
+			'/pokemon/:id': function (id) {
+				pokemonID = parseInt(id);
+				if (pokedex != null) renderPokemon(pokemonID);
+			}
+		}
+
+		var router = Router(routes);
+		router.init();
+	}
+	initRouter();
 
 	// Make call to Pokedex API
 	var requestStream = Rx.Observable.just('api/pokemon.json');
@@ -11,9 +28,13 @@ $(function() {
 
 	// Subscribe to data from Pokedex API call
 	responseStream.subscribe(function (res) {
-		rivets.bind($('#pokemon'), {pokemon: res[0]});
-		res.forEach(function (pokemon) {
-			// Do something with each pokemon
-		});
+		pokedex = res;
+		renderPokemon(pokemonID);
 	});
+
+	function renderPokemon(id) {
+		i = id - 1;
+		if (template == null) template = rivets.bind($('#pokemon'), {pokemon: pokedex[i]});
+		else template.update({pokemon: pokedex[i]});
+	}
 });
